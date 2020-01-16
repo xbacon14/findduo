51c58d7e1be930f9eed4821eeb15eb243af5673a
xbacon14 <italo.galeano123@gmail.com>
2020-01-16 11:45:01 -0300

    Updates

---
+++ precache-manifest.bc1f33433e439d8453aa7f5dcb9ca8e4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a66ae4430dc1ed393489b9d5713a37bc",
+    "url": "/findduo/index.html"
+  },
+  {
+    "revision": "7653880728396a10fc2a",
+    "url": "/findduo/static/css/main.fddd73bc.chunk.css"
+  },
+  {
+    "revision": "52ee1542b06f01074939",
+    "url": "/findduo/static/js/2.1076647d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/findduo/static/js/2.1076647d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7653880728396a10fc2a",
+    "url": "/findduo/static/js/main.cb7c70a0.chunk.js"
+  },
+  {
+    "revision": "9bacd96345a8af2fa962",
+    "url": "/findduo/static/js/runtime-main.d4f84d0e.js"
+  },
+  {
+    "revision": "6f6e1bdfb20107cbd8410beffecd72fc",
+    "url": "/findduo/static/media/Like.6f6e1bdf.png"
+  },
+  {
+    "revision": "d373962a57cc5bfe93e5032a575f1016",
+    "url": "/findduo/static/media/LogoMain.d373962a.png"
+  },
+  {
+    "revision": "a07405187b5a325659a2e78a04da37fc",
+    "url": "/findduo/static/media/Nlike.a0740518.png"
+  },
+  {
+    "revision": "339ab785663d8f74398a035c2fdac299",
+    "url": "/findduo/static/media/brand.339ab785.png"
+  },
+  {
+    "revision": "753a5d647fac4c23d49e14881c350c22",
+    "url": "/findduo/static/media/logo.753a5d64.png"
+  }
+]);
